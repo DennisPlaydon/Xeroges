c7ab585dacf835f357b9cdb58c6d81300ea4cc33
Josh Jacobsen <josh.jacobsen@xero.com>
2018-09-07 16:53:49 +1200

    initial commit

---
+++ Meshd/Meshd/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Meshd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
